e66fdee4e61837b46139c26b467372a489f769ec
xfu <angerpeanut@gmail.com>
2023-03-19 16:45:44 +0800

    remove ant api

--- antui/src/services/swagger/index.ts
+++ antui/src/services/swagger/index.ts
@@ -2,11 +2,5 @@
 /* eslint-disable */
 // API 更新时间：
 // API 唯一标识：
-import * as pet from './pet';
-import * as store from './store';
-import * as user from './user';
-export default {
-  pet,
-  store,
-  user,
-};
+
+export default {};
