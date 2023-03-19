027867ea98b2702177e5a40bce8452b501f74697
xfu <angerpeanut@gmail.com>
2023-03-19 17:30:22 +0800

    Update config.ts

--- antui/config/config.ts
+++ antui/config/config.ts
@@ -67,20 +67,7 @@ export default defineConfig({
   },
   // Fast Refresh 热更新
   fastRefresh: {},
-  openAPI: [
-    {
-      requestLibPath: "import { request } from 'umi'",
-      // 或者使用在线的版本
-      // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
-      schemaPath: join(__dirname, 'oneapi.json'),
-      mock: true,
-    },
-    {
-      requestLibPath: "import { request } from 'umi'",
-      schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
-      projectName: 'swagger',
-    },
-  ],
+  openAPI: [],
   nodeModulesTransform: {
     type: 'none',
   },
