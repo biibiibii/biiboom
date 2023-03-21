49af4e7d2d4525d04d901f8698f0c8fb780b8135
xfu <angerpeanut@gmail.com>
2023-03-21 21:57:49 +0800

    Update service.ts

--- antui/src/pages/forum/service.ts
+++ antui/src/pages/forum/service.ts
@@ -4,7 +4,7 @@ import type { SiteData } from './data';
 export async function querySiteNodes(): Promise<{ data: { site: SiteData[] } }> {
   const payload = {
     query:
-      'query GetMatchRule {\n  site(limit: 20) {\n    name\n    url\n    rule_id\n    jump_base_url\n    id\n    nodes(limit: 20, order_by: {posted_at: desc}) {\n      url\n      title\n      site_id\n      posted_at\n      id\n      extra\n      desc\n      _updated_at\n    }\n  }\n}\n',
+      'query GetMatchRule {\n  site(limit: 30) {\n    name\n    url\n    rule_id\n    jump_base_url\n    id\n    nodes(limit: 30, order_by: {posted_at: desc}) {\n      url\n      title\n      site_id\n      posted_at\n      id\n      extra\n      desc\n      _updated_at\n    }\n  }\n}\n',
     variables: null,
     operationName: 'GetMatchRule',
   };
